499d5a6a7180e39a332783e6c47aab692115fc9a
rpinto <rachel.lea.pinto@gmail.com>
2018-07-24 13:02:23 -0700

    hourly rate drill

--- hourly-rate-calc-drill/src/components/number-input.js
+++ hourly-rate-calc-drill/src/components/number-input.js
@@ -3,8 +3,15 @@ import React from 'react';
 export default function NumberInput(props) {
     return (
         <div className="form-group">
-            <label htmlFor={props.id}>{props.label}</label>
-            <input type="number" id={props.id} min={props.min} max={props.max} />
+            <label 
+                htmlFor={props.id}>{props.label}
+            </label>
+            <input 
+                type="number" 
+                id={props.id} 
+                min={props.min} max={props.max} 
+                onChange={e => props.someMethod(e.target.value)}
+            />
         </div>
     ); 
 }
--- hourly-rate-calc-drill/src/components/rate-calculator.js
+++ hourly-rate-calc-drill/src/components/rate-calculator.js
@@ -5,26 +5,50 @@ import Output from './output';
 
 export default class RateCalculator extends React.Component {
     constructor(props) {
-        super(props);
-      
-//         this.state = {
-            
-//         }
         
+        super(props);
+        this.state = {
+            hours : 15,
+            dayRate : 5
+        }
     }
-  
+
+    dayRateMethod() {
+        this.setState({
+            dayRate : NumberInput
+        })
+    }
+
+    hoursMethod() {
+        this.setState({
+            hours : NumberInput
+        })
+    }
+    
   
 
     render() {
       
-        const rate = dayRate/hours;
-      
+        const rate = this.state.dayRate / this.state.hours;
+        console.log(this.state.dayRate, this.state.hours, rate, 'these are values');
         return (
             <form>
-                <NumberInput id="day-rate" label="Day rate" min={0} max={5000} />
-                <NumberInput id="hours" label="Hours" min={1} max={12} />
-                <Output id="hourly-rate" label="Hourly rate" value={rate.toFixed(2)}
-/>
+                <NumberInput 
+                    id="day-rate" 
+                    label="Day rate" 
+                    min={0} max={5000}
+                    someMethod={e => this.dayRateMethod(e)} />
+                    
+                <NumberInput 
+                    id="hours" 
+                    label="Hours" 
+                    min={1} max={12} 
+                    someMethod={e => this.hoursMethod(e)}/>
+                <Output 
+                    id="hourly-rate" 
+                    label="Hourly rate" 
+                    value={rate.toFixed(2)} 
+                />
             </form>
         );
     }
