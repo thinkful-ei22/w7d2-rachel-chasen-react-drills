317078fee785d2a0b5c61e21fdd22f6154d9ceb9
ccareyncsu <ccarey@ncsu.edu>
2018-07-24 13:24:25 -0600

    rate calc

---
+++ hourly-rate-calc-drill/src/components/rate-calculator.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+import NumberInput from './number-input';
+import Output from './output';
+
+export default class RateCalculator extends React.Component {
+    constructor(props) {
+        super(props);
+      
+//         this.state = {
+            
+//         }
+        
+    }
+  
+  
+
+    render() {
+      
+        const rate = dayRate/hours;
+      
+        return (
+            <form>
+                <NumberInput id="day-rate" label="Day rate" min={0} max={5000} />
+                <NumberInput id="hours" label="Hours" min={1} max={12} />
+                <Output id="hourly-rate" label="Hourly rate" value={rate.toFixed(2)}
+/>
+            </form>
+        );
+    }
+}
+
